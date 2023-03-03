7199df6605146f13b5044b943063fa6c1c7ad4b2
dimaqqw <dimaaqqw@mail.ru>
2023-03-03 16:37:44 +0300

    l5 start package

---
+++ vue-flat-shop/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <p>Меня зовут {{ msg }}</p>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        msg: String
+    }
+}
+</script>
+<style>
+    p{
+        color: red;
+    }
+</style>
